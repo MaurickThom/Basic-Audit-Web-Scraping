e574b16127d13f13aaf181dc3428e336df0592cc
MaurickThom <thomtwd@gmail.com>
2019-08-12 22:49:36 -0500

    [adding the function pageSize]

--- seo.py
+++ seo.py
@@ -5,5 +5,10 @@ def verifySSL(URL):
     result = request.urlopen(req)
     return result.geturl()
 
+def pageSize(URL):
+    site = request.urlopen(URL)
+    meta = site.info()
+    return {f'Content-Length of {site.geturl()}':site.headers['content-length']}
+
 if __name__ == "__main__":
-    print(verifySSL('http://python.org'))
+    print(pageSize('http://python.org'))
