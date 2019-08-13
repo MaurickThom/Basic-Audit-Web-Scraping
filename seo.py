e4a44125d7bd765c575796e607c212224862849d
MaurickThom <thomtwd@gmail.com>
2019-08-13 00:33:08 -0500

    [function comparisons]

--- seo.py
+++ seo.py
@@ -1,4 +1,6 @@
 from urllib import request
+import os
+from bs4 import BeautifulSoup
 
 def verifySSL(URL):
     req = request.Request(URL)
@@ -8,7 +10,29 @@ def verifySSL(URL):
 def pageSize(URL):
     site = request.urlopen(URL)
     meta = site.info()
-    return {f'Content-Length of {site.geturl()}':site.headers['content-length']}
+    try:
+        return {f'Content-Length of {site.geturl()}':site.headers['content-length']}
+    finally:
+        site.close()
+
+def verifyWWW(URL):
+    req = request.Request(URL)
+    result = request.urlopen(req)
+    return result.geturl()
+
+def comparisons(URL):
+    site = request.urlopen(URL)
+    meta = site.info() # metadata HTML
+    file = open('out.txt','wb')
+    file.write(site.read())
+    site.close()
+    file.close()
+    file = open('out.txt','r')
+    print(f'File on disk after download {len(file.read())}')
+    file.close()
+    print(f'os.stat().st_size returns : ',os.stat('out.txt').st_size)
 
 if __name__ == "__main__":
-    print(pageSize('http://python.org'))
+    URL = 'http://python.org'
+    print(pageSize(URL))
+    comparisons(URL)
