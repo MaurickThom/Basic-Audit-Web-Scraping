10f2953eac1d8d6a1df365ee903627e34615ba42
MaurickThom <thomtwd@gmail.com>
2019-08-21 22:54:46 -0500

    keywords

--- seo.py
+++ seo.py
@@ -2,27 +2,33 @@ from urllib import request
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import os
+import re
 
 def verifySSL(URL):
-    req = request.Request(URL)
-    result = request.urlopen(req)
+    # req = request.Request(URL)
+    # result = request.urlopen(req)
+    result = urlopen(URL)
     return result.geturl()
 
 def pageSize(URL):
-    site = request.urlopen(URL)
+    # site = request.urlopen(URL)
+    site = urlopen(URL)
     meta = site.info()
+    print(f'Meta :  \n{meta}')
     try:
         return {f'Content-Length of {site.geturl()}':site.headers['content-length']}
     finally:
         site.close()
 
 def verifyWWW(URL):
-    req = request.Request(URL)
-    result = request.urlopen(req)
+    # req = request.Request(URL)
+    # result = request.urlopen(req)
+    result = urlopen(URL)
     return result.geturl()
 
 def comparisons(URL):
-    site = request.urlopen(URL)
+    # site = request.urlopen(URL)
+    site = urlopen(URL)
     meta = site.info() # metadata HTML
     file = open('page.txt','wb')
     file.write(site.read())
@@ -34,7 +40,8 @@ def comparisons(URL):
     print(f'os.stat().st_size returns : ',os.stat('page.txt').st_size)
     
 def verifyDescription(URL):
-    site = request.urlopen(URL)
+    # site = request.urlopen(URL)
+    site = urlopen(URL)
     soup = BeautifulSoup(site)
     description = soup.find('meta',attrs={'name':'description'}) # que busque en el atributo name="description"
     site.close()
@@ -50,7 +57,21 @@ def verifyTitle(URL):
     print(f'the size of the title content is {len(soup.html.head.title.string)}')
     print(f'the content of the title is {soup.html.head.title.string}')
 
-def keywords(URL):
+def keywords(URL): #palabras clave de mayor densidad
+    site = urlopen(URL)
+    soup = BeautifulSoup(site)
+    keyWords = soup.find('meta',attrs={'name':'keywords'})
+    print(f'keywords {keyWords}')
+    words = keyWords.get('content').split()
+    # print(len(words))
+    for word in words :
+        print(f'the word {word} is repeated {len(soup.findAll(text=re.compile(word)))} times')
+    
+# check the alt attribute in the image tag
+def checkTag(URL):
+    pass
+
+def verifyH1(URL):
     pass
 
 if __name__ == "__main__":
